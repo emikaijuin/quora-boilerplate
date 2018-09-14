b82d237675cde9acc5f7c4bd71cef6651145ace7
Emika Ijuin <emikaijuin@gmail.com>
2018-09-14 13:00:22 +0800

    Users have many questions

--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   ## Attribute Validations
   validates :email, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
+  ## BCrypt Password 
   has_secure_password
+  ## Associations
+  has_many :questions
 end
